fa31af947231dab01073a76184ded0510b6ef7b5
Kadir Zukorlic <kadirzukorlic@gmail.com>
2024-02-04 05:31:35 +0100

    Create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepository.create({
+      email,
+      password,
+    });
+    return this.userRepository.save(user);
+  }
+}
