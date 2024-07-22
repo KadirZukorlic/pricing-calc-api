2b1d69091963d824d0380f226fd66394c76bd9b5
KadirZukorlic <kadirzukorlic@gmail.com>
2024-07-23 01:49:08 +0200

    Create report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './reports.entity';
 
 @Injectable()
 export class ReportsService {
-  async create(body: any) {
-    console.log('Creating a report...', body);
+  constructor(
+    @InjectRepository(Report) private reportsRepository: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.reportsRepository.create(reportDto);
+
+    return this.reportsRepository.save(report);
   }
 }
