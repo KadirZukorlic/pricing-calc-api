57e9c91de23600da9c84e68e556880144681bedb
KadirZukorlic <kadirzukorlic@gmail.com>
2024-07-22 18:30:16 +0200

    Add CreateReport rout and service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  async create(body: any) {
+    console.log('Creating a report...', body);
+  }
+}
