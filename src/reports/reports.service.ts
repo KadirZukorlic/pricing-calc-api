4220a45e27de98b840f9ca5f442e1965ce3f4d3c
KadirZukorlic <kadirzukorlic@gmail.com>
2024-07-24 03:10:50 +0200

    Add Patch route to approve a report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -14,6 +14,20 @@ export class ReportsService {
   create(reportDto: CreateReportDto, user: User) {
     const report = this.reportsRepository.create(reportDto);
     report.user = user;
+
+    return this.reportsRepository.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.reportsRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
 
     return this.reportsRepository.save(report);
   }
