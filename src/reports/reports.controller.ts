fc3e6f1f039465a75e0665f58178c2216b402c3c
KadirZukorlic <kadirzukorlic@gmail.com>
2024-07-27 03:20:06 +0200

    Add CurrentUserMiddleware to all routes

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -14,6 +14,7 @@ import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -26,6 +27,7 @@ export class ReportsController {
     this.reportsService.create(body, user);
   }
 
+  @UseGuards(AdminGuard)
   @Patch('/:id')
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
